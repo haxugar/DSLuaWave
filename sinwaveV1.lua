01ddff5ee0bf41a7b2af3d71cd6a1ff74bd46ad5
haxugar <haxugar@yahoo.com>
2015-07-01 12:53:17 +0100

    license update

--- sinwaveV1.lua
+++ sinwaveV1.lua
@@ -1,11 +1,19 @@
 -- DSLuaWave
--- See github for license
+--[[ 
+Copyright 2015 "haxugar"
+
+Licensed under the Egreen License, Version 1.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+http://www.Egreen.io/licenses/LICENSE-1.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+]]--
 
 --Declaring variables
 valuea = 0
 y = 0
 x = 0
-input1 = 0.1
+input1 = 0.2
 input2 = 72
 
 --Declaring color variables
@@ -18,13 +26,14 @@ while not Keys.held.Start do
 	Controls.read()
 	
 	if x > 256 then
-		valuea = 0
+		x = 0
 		end
 	
 	--Calculating each values and adapting them to the screen size
-	valuea = valuea + input1
-	x = valuea * 20
-	y = math.floor(input2*math.sin(valuea)+96)
+	--valuea = valuea + input1
+	--x = valuea * 20
+	x = x + input1
+	y = math.floor(input2*math.sin(x)+96)
 	
 	--Drawing blue horizontal and vertical lines
 	screen.drawFillRect(SCREEN_UP, x, 0, x + 1, 192, blue)
@@ -37,8 +46,8 @@ while not Keys.held.Start do
 	screen.print(SCREEN_DOWN, 2  , 2  , "f(x)     =")
 	screen.print(SCREEN_DOWN, 65 , 2  , "72*sin(x)+96", red)
 	screen.print(SCREEN_DOWN, 2  , 11 , "f(")
-	screen.print(SCREEN_DOWN, 15 , 11 , valuea, red)
-	screen.print(SCREEN_DOWN, 44 , 11 , ") = ")
+	screen.print(SCREEN_DOWN, 15 , 11 , x, red)
+	screen.print(SCREEN_DOWN, 44 , 11 , " )= ")
 	screen.print(SCREEN_DOWN, 65 , 11 , y, red)
 	screen.print(SCREEN_DOWN, 2 , 180 , "github.com/haxugar/DSLuaWave", green)
 	
